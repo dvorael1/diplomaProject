79ed62e6cfa861db0d71832b19fc2ae59a088f0e
eliska <dvorael@fel.cvut.cz>
2020-06-10 18:11:57 +0200

    10th June normalization

--- data_load.R
+++ data_load.R
@@ -1,62 +1,124 @@
+source("normalize.R")
+
+plot_scRNA = function(x,y=NULL,xlab = "",ylab = "",main = ""){
+  mx = max(x)+1
+  my = max(y)+1
+  if(!is.null(y)){
+    exclude = x == y & x == 0
+    if(sum(exclude)>0){
+      i = match(TRUE,exclude)
+      exclude[i[1]] = FALSE
+    }
+    plot(x[!exclude],y[!exclude],main=main,xlab = xlab,ylab = ylab, xlim = c(0,mx), ylim=c(0,my))
+  }else{
+    plot(x,main=main,xlab = xlab,ylab = ylab, xlim = c(0,mx), ylim=c(0,mx))
+  }
+}
+
+preprocess = function(data, csize = 600, rsize = 10){
+  keep_feature <- rowSums(data) > 0
+  data <- data[keep_feature, ]
+  
+  keep_cell <- colSums(data) > 0
+  data <- data[ ,keep_cell]
+  
+  cols = sample.int(dim(data)[2], csize)
+  data <- data[ ,cols]
+  
+  rows = sample.int(dim(data)[1], rsize)
+  data <- data[rows, ]
+
+  keep_feature <- rowSums(data) > 0
+  data <- data[keep_feature, ]
+  
+  keep_cell <- colSums(data) > 0
+  data <- data[ ,keep_cell]
+  return(data)
+}
 
 files <- list.files(path = "C:/Users/Ela/Desktop/owncloud/documents/documents/BIN/project/GSE139369", pattern = "\\.rds$", full.names = TRUE)
 # files <- list.files(path = "/media/eliska/FC089B5C089B152C/Users/Ela/Desktop/owncloud/documents/documents/BIN/project/GSE139369", pattern = "\\.rds$", full.names = TRUE)
 r <- lapply(files, readRDS)
 # # v = 1:(length(r)/2)
 v = 1:1
+
 i=1
 dataset = list()
+setwd("graphs")
 for (i in v) {
   name = substr(files[2*i], 90, 98)
   t1 = r[[2*i-1]]
   t2 = r[[2*i]]
-
+  
   t1[is.na(t1)] <- 0
   t2[is.na(t2)] <- 0
   
-  t3 <- SingleCellExperiment(
-    assays = list(counts = t1), 
-  )
-  t4 <- SingleCellExperiment(
-    assays = list(counts = t2), 
-  )
+  raw = preprocess(t2,csize=600,rsize=20)
   
-  keep_feature <- rowSums(counts(t3) > 0) > 0
-  t3 <- t3[keep_feature, ]
-  
-  keep_feature <- rowSums(counts(t4) > 0) > 0
-  t4 <- t4[keep_feature, ]
-  
-  t3 = scater::runPCA(t1,use_coldata = TRUE, detect_outliers = TRUE)
-  
-  t4 = runPCA(t2, use_coldata = TRUE, detect_outliers = TRUE)
-  
-  
-  reducedDimNames(t3)
-  reducedDimNames(t4)
+  pdf("normalize_example.pdf",5,4 )
+  plot(raw,main="raw",xlab = "cells",ylab = "genes")
 
-#   for (row1 in row.names(t1)){
-#     for (row2 in row.names(t2)[101:500]){
-#       tbl_data = list()
-#       tbl_data[[1]] = t1[row1,]
-#       tbl_data[[2]] = t2[row2,]
-#       dataset[[ paste( row1, row2, name, sep=" x ")]] = tbl_data
-#       if(i>=100){
-#         break
-#       }
-#     }
-#   }
+  cmp = calc_cpm(raw)
+  plot(cmp,main="cmp",xlab = "cells",ylab = "genes")
 
-  }
+  sf = calc_sf(raw)
+  plot(sf,main="sf",xlab = "cells",ylab = "genes")
 
-for( i in names(dataset)){
-  t = strsplit(i,"x")
-  g1 = t[[1]][1] 
-  g2 = t[[1]][2]
-  if(length(dataset[[i]][[1]][dataset[[i]][[1]]!=0])>=1 && length(dataset[[i]][[2]][dataset[[i]][[2]]!=0])>=1){
+  uq = calc_uq(raw)
+  plot(uq,main="uq",xlab = "cells",ylab = "genes")
+
+  dsm = Down_Sample_Matrix(raw)
+  plot(dsm,main="Down_Sample_Matrix",xlab = "cells",ylab = "genes")
+
+  # scran_norm = scran_normilize(raw)
+  # plot(log2(scran_norm),main="scran_normilize",xlab = "cells",ylab = "genes")
+
+
+  dev.off()
+
+  
+  for (row1 in row.names(raw)){
+    for (row2 in row.names(raw)){
     
-    pdf(paste( i, ".pdf", sep=""))
-    plot(dataset[[i]][[1]],dataset[[1]][[2]], main = i, xlab = g1, ylab = g2 )
-    dev.off()
+      pdf(paste(paste(row1,row2,sep="_"),"pdf",sep="."),5,4 )
+      
+      x = raw[row1,]
+      y = raw[row2,]
+      
+      
+      plot_scRNA(x,y,main="raw",xlab = row1,ylab = row2)
+      
+      x = cmp[row2,]
+      y = cmp[row2,]
+      plot_scRNA(x,y,main="cmp",xlab = row1,ylab = row2)
+      
+      x = sf[row2,]
+      y = sf[row2,]
+      plot_scRNA(x,y,main="sf",xlab = row1,ylab = row2)
+      
+      x = uq[row2,]
+      y = uq[row2,]
+      plot_scRNA(x,y,main="uq",xlab = row1,ylab = row2)
+      
+      x = dsm[row2,]
+      y = dsm[row2,]
+      plot_scRNA(x,y,main="Down_Sample_Matrix",xlab = row1,ylab = row2)
+      
+      dev.off()
+    }
   }
+  
 }
+setwd("..")
+
+# for( i in names(dataset)){
+#   t = strsplit(i,"x")
+#   g1 = t[[1]][1] 
+#   g2 = t[[1]][2]
+#   if(length(dataset[[i]][[1]][dataset[[i]][[1]]!=0])>=1 && length(dataset[[i]][[2]][dataset[[i]][[2]]!=0])>=1){
+#     
+#     pdf(paste( i, ".pdf", sep=""))
+#     plot(dataset[[i]][[1]],dataset[[1]][[2]], main = i, xlab = g1, ylab = g2 )
+#     dev.off()
+#   }
+# }
--- normalize.R
+++ normalize.R
@@ -5,20 +5,65 @@
 # umi.qc <- umi[rowData(umi)$use, colData(umi)$use]
 # endog_genes <- !rowData(umi.qc)$is_feature_control
 
-M <- matrix(1:9, nrow=3, byrow=TRUE)
 
-cpm_normalize = function(col){
-  s = sum(col)
-  col = 1000000*col
-  col = col/s
-  return(col)
+
+calc_cpm <- function (expr_mat, spikes = NULL) # spikes must be integer, not logical vector
+{ 
+  norm_factor <- colSums( expr_mat ) 
+  return( t( t( expr_mat ) / norm_factor ) ) * 10^6 
 }
 
-rpk_normalize = function(col){
-  s = sum(col)
-  col = 1000*col
-  col = col/s
-  return(col)
+calc_sf <- function (expr_mat) { 
+  gm <- function(cnts) { 
+    exp( mean( log( cnts[!(cnts==0)] ) ) ) 
+  }
+  geomeans <- apply( expr_mat, 1, gm) 
+  SF <- function(cnts) { 
+    tmp = ( ( cnts / geomeans )[ (is.finite(geomeans) & geomeans > 0) ] )
+    median( tmp[tmp>0] ) 
+  }
+  norm_factor <- apply( expr_mat, 2, SF) 
+  keep_cols = norm_factor > 0
+  expr_mat[,keep_cols] = t( t( expr_mat[,keep_cols] ) / norm_factor ) 
+  return(expr_mat)
 }
 
-t = apply(M,1,rpk_normalize)
+calc_uq <- function (expr_mat, quantile=0.99) 
+{ 
+  UQ <- function(x) { quantile( x[ x > 0 ], quantile ) }
+  keep_cols = colSums(expr_mat) > 0
+  non_zero = expr_mat[,keep_cols]
+  uq <- unlist( apply( non_zero, 2, UQ ) ) 
+  
+  norm_factor <- uq / median(uq) 
+  result = ( t( t( non_zero ) / norm_factor) ) 
+  expr_mat[,keep_cols] = result
+  return(expr_mat)
+} 
+
+Down_Sample_Matrix <- function(expr_mat) {
+  keep_cols = colSums(expr_mat) > 0
+  non_zero = expr_mat[,keep_cols]
+  min_lib_size <- min(colSums(non_zero))
+  down_sample <- function(x) {
+    prob <- min_lib_size/sum(x)
+    return(
+      sapply(x, function(y) { rbinom(1, y, prob) } )
+    ) }
+  down_sampled_mat <- apply(non_zero, 2, down_sample)
+  expr_mat[,keep_cols] = down_sampled_mat
+  return(expr_mat)
+}
+
+scran_normilize <- function(expr_mat){
+  require(scran, quietly = TRUE)
+  keep_cols = colSums(expr_mat) > 0
+  non_zero = expr_mat[,keep_cols]
+  qclust <- quickCluster(non_zero, min.size = 30)
+  non_zero <- computeSumFactors(non_zero, sizes = 15, clusters = qclust)
+  expr_mat[,keep_cols] <- normalize(non_zero)
+  return(expr_mat)
+}
+
+# scran_normilize(matrix(raw,20,600))
+test = Down_Sample_Matrix(raw)
